05045451a1d0082269a1b5b38d432d61f001bf1b
ardit <chethanaradha98@gmail.com>
2023-01-16 13:19:29 +0530

    Init from VS Code

---
+++ coding exercise/Coding Exercise 1.py
@@ -0,0 +1,10 @@
+text = "Hello World!.."
+print(type(text))
+
+
+message = input("Enter your message here: ")
+print(message)
+
+
+list_strings = ["string1", "string2", "String3", "string4"]
+print(list_strings)
